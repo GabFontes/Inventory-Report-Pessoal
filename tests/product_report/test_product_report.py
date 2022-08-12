84f71e9b7100e7a1e74cfeafe21fe5409a2c9b98
gab <gabrielfontes321@gmail.com>
2022-08-12 15:50:11 -0300

    feat(tests/product_report): implements individual product report test (requirement 2)

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        000,
+        "Cachaça",
+        "Bar do seu Zé",
+        "03/03/2003",
+        "03/03/2013",
+        "333",
+        "...",
+    )
+
+    string = 'O produto Cachaça fabricado em '\
+        '03/03/2003 por Bar do seu Zé com validade até '\
+        '03/03/2013 precisa ser armazenado ....'
+
+    message = False
+
+    if string in str(product.__repr__):
+        message = True
+
+    assert message is True
