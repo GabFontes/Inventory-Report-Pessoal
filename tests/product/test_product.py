03f7e2e993a6cdc18db2850b97639fd976b129f5
gab <gabrielfontes321@gmail.com>
2022-08-12 15:24:26 -0300

    feat(tests/product): implements initializer/constructor test (requirement 1)

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        000,
+        "Cachaça",
+        "Bar do seu Zé",
+        "03/03/2003",
+        "03/03/2013",
+        "333",
+        "...",
+    )
+    assert product.id == 000
+    assert product.nome_do_produto == "Cachaça"
+    assert product.nome_da_empresa == "Bar do seu Zé"
+    assert product.data_de_fabricacao == "03/03/2003"
+    assert product.data_de_validade == "03/03/2013"
+    assert product.numero_de_serie == "333"
+    assert product.instrucoes_de_armazenamento == "..."
